4d64e5300a45c6a9a82c00c02826c32666bfba24
Klemens Starybrat <klemens.starybrat@gmail.com>
2018-07-19 21:23:00 +0200

    refactor: Add details page to MVC

--- js/dbhelper.js
+++ js/dbhelper.js
@@ -135,7 +135,7 @@ class DBHelper {
    * Restaurant page URL.
    */
   static urlForRestaurant(restaurant) {
-    return (`./restaurant.html?id=${restaurant.id}`);
+    return (`./restaurant/?id=${restaurant.id}`);
   }
 
   /**
--- js/main.js
+++ js/main.js
@@ -172,10 +172,10 @@ const mapModel = {
   markers: [],
 
   // Mapbox init
-  init() {
+  init(center, zoom) {
     this.newMap = L.map('map', {
-      center: [40.722216, -73.987501],
-      zoom: 12,
+      center: center,
+      zoom: zoom,
       scrollWheelZoom: false
     });
     L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.jpg70?access_token={mapboxToken}', {
@@ -203,6 +203,10 @@ const mapModel = {
     });
   },
 
+  addMarker(restaurant) {
+    DBHelper.mapMarkerForRestaurant(restaurant);
+  },
+
   resetMarkers() {
     if (this.markers) {
       this.markers.forEach(marker => marker.remove());
@@ -220,7 +224,8 @@ const controller = {
   init() {
     filterModel.init().then(() => filterView.init());
     restaurantsModel.init().then(() => restaurantsView.init());
-    mapModel.init();
+    mapModel.init([40.722216, -73.987501], 12);
+    breadcrumbView.reset();
   },
 
   // Getter methods
@@ -297,6 +302,157 @@ const mainView = {
     this.el.innerHTML = this.getTemplate(state).innerHTML;
 
     // Proceed with subviews
-    controller.init();
+    switch (state) {
+    case 'index':
+      controller.init();
+      break;
+    case 'details':
+      detailsController.init();
+      break;
+    }
   }
+};
+
+const restaurantModel = {
+  init() {
+    return this.fetchFromURL();
+  },
+
+  fetchFromURL() {
+    return new Promise((resolve, reject) => {
+      const id = getParameterByName('id');
+      if (this.restaurant && this.restaurant.id === id) { // restaurant already fetched!
+        resolve(this.restaurant);
+      }
+      if (!id) { // no id found in URL
+        reject('No restaurant id in URL');
+      } else {
+        DBHelper.fetchRestaurantById(id).then(restaurant => {
+          this.restaurant = restaurant;
+          if (!restaurant) {
+            reject(`No restaurant with id ${id} found.`);
+          }
+          // fillRestaurantHTML();
+          resolve(restaurant);
+        });
+      }
+    });
+  }
+};
+
+const restaurantView = {
+  render(restaurant) {
+    const name = document.getElementById('restaurant-name');
+    name.innerHTML = restaurant.name;
+
+    const address = document.getElementById('restaurant-address');
+    address.innerHTML = restaurant.address;
+
+    const image = document.getElementById('restaurant-img');
+    image.className = 'restaurant-img'
+    image.src = DBHelper.imageUrlForRestaurant(restaurant);
+
+    const cuisine = document.getElementById('restaurant-cuisine');
+    cuisine.innerHTML = restaurant.cuisine_type;
+
+    // fill operating hours
+    if (restaurant.operating_hours) {
+      this.renderHours(restaurant.operating_hours);
+    }
+    // fill reviews
+    this.renderReviews(restaurant.reviews);
+  },
+
+  renderHours(operatingHours) {
+    const hours = document.getElementById('restaurant-hours');
+    for (let key in operatingHours) {
+      const row = document.createElement('tr');
+
+      const day = document.createElement('td');
+      day.innerHTML = key;
+      row.appendChild(day);
+
+      const time = document.createElement('td');
+      time.innerHTML = operatingHours[key];
+      row.appendChild(time);
+
+      hours.appendChild(row);
+    }
+  },
+
+  renderReviews(reviews) {
+    const container = document.getElementById('reviews-container');
+    const title = document.createElement('h2');
+    title.innerHTML = 'Reviews';
+    container.appendChild(title);
+
+    if (!reviews) {
+      const noReviews = document.createElement('p');
+      noReviews.innerHTML = 'No reviews yet!';
+      container.appendChild(noReviews);
+      return;
+    }
+    const ul = document.getElementById('reviews-list');
+    reviews.forEach(review => {
+      ul.appendChild(this.createReviewHTML(review));
+    });
+    container.appendChild(ul);
+  },
+
+  createReviewHTML(review) {
+    const li = document.createElement('li');
+    const name = document.createElement('p');
+    name.innerHTML = review.name;
+    li.appendChild(name);
+
+    const date = document.createElement('p');
+    date.innerHTML = review.date;
+    li.appendChild(date);
+
+    const rating = document.createElement('p');
+    rating.innerHTML = `Rating: ${review.rating}`;
+    li.appendChild(rating);
+
+    const comments = document.createElement('p');
+    comments.innerHTML = review.comments;
+    li.appendChild(comments);
+
+    return li;
+  }
+
+};
+
+const detailsController = {
+  init() {
+    restaurantModel.init().then(restaurant => {
+      mapModel.init([restaurant.latlng.lat, restaurant.latlng.lng], 16);
+      mapModel.addMarker(restaurant);
+      breadcrumbView.render(restaurant);
+      restaurantView.render(restaurant);
+    });
+  }
+};
+
+const breadcrumbView = {
+  el: document.getElementById('breadcrumb'),
+
+  render(restaurant) {
+    const li = document.createElement('li');
+    li.innerHTML = restaurant.name;
+    this.el.appendChild(li);
+  },
+
+  reset() {
+    this.el.innerHTML = '<li><a href="/">Home</a></li>';
+  }
+};
+
+/**
+ * Get a parameter by name from page URL.
+ */
+const getParameterByName = (name, url) => {
+  if (!url)
+    url = window.location.href;
+  url = new URL(url);
+  return new URLSearchParams(url.search).get('id');
 };
