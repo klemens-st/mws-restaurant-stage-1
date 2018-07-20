804d86c6980a5d3e6d0f9dd981260859a56c159b
Klemens Starybrat <klemens.starybrat@gmail.com>
2018-07-20 11:27:19 +0200

    feat: Add responsive styles for details page

--- js/main.js
+++ js/main.js
@@ -425,10 +425,10 @@ const restaurantView = {
 const detailsController = {
   init() {
     restaurantModel.init().then(restaurant => {
-      mapModel.init([restaurant.latlng.lat, restaurant.latlng.lng], 16);
-      mapModel.addMarker(restaurant);
       breadcrumbView.render(restaurant);
       restaurantView.render(restaurant);
+      mapModel.init([restaurant.latlng.lat, restaurant.latlng.lng], 16);
+      mapModel.addMarker(restaurant);
     });
   }
 };
